22decd92d2130b6d7cf1e5496f1be62c6d0aff2e
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2023-12-25 01:17:17 +0530

    Revert "Updated About"
    This reverts commit ba06892f97827395d510b69a2b0ba79ceb19e716.

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-[82vh] p-3 bg-white dark:bg-[#282A36] dark:text-white'>
-      <h1 className='text-3xl font-semibold mb-10'>Blogs</h1>
-      <p className='text-center text-xl font-sans px-4 md:px-16 lg:px-32 xl:px-64'>
-       This Section will contains blogs about all the updates.
-      </p>
+    <div className='flex justify-center font-semibold text-3xl w-full h-[82vh] dark:text-white dark:bg-[#282A36]'>
+    <h1>Blogs</h1>
     </div>
   )
 }
