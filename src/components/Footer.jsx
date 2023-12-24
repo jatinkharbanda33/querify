d4ad9fbab4ca9d18e62a03b8c8b650e5c7b89a99
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2023-12-24 18:37:38 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	const year = new Date().getFullYear();
 	return (
-	  <footer className={`footer flex items-center justify-center p-2 mb-0 dark:bg-gray-800 dark:text-white bg-gray-200 text-gray-700 inset-x-0`}>
+	  <footer className={`footer flex items-center justify-center p-2 mb-0 mt-7 dark:bg-gray-800 dark:text-white bg-gray-200 text-gray-700 inset-x-0`}>
 		<div>
 		  <p>Copyright © {year} - Made by Jatin Kharbanda (for Atlan Assesment)</p>
 		</div>
